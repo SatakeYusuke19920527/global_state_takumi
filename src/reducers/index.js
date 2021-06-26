c7426ef04ae8d09a1ce27e4b545c22078e69b14f
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-06-26 20:58:27 +0900

    表示を5人までにした

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,14 @@ import { GET_DATA } from '../actions/index'
 const reducer = (state, action) => {
   switch (action.type) {
     case GET_DATA:
-      return { ...state, user_data: action.data }
+      const result = action.data.filter(user => user.id <= 5)
+    //   const namae = () => {
+    //     state.user.id <= 5
+    // }
+      console.log(result)
+      console.log(action.data)
+      console.log(state.user_data)
+      return { ...state, user_data: result }
     default:
       return state
   }
